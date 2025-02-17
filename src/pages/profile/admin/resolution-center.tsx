0eccc885da5668ed5705da424ebd61ee0b3122fd
Brachi Yitzchaki <bracha44948@gmail.com>
2025-02-18 00:12:21 +0200

    add all admin panel pages

---
+++ src/pages/profile/admin/resolution-center.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import LayoutWrapper from "@/components/Layout";
+
+
+const ResolutionCenter = () => {
+
+    return (
+        <LayoutWrapper>
+            <div>
+                Resolution Center - not implemented yet
+            </div>
+        </LayoutWrapper>
+    )
+};
+export default ResolutionCenter;
